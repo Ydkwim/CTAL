58df905cd8093456e2c872830521ea483467933b
Harvey <lihang4@100tal.com>
2021-02-16 10:44:44 +0800

    code upgrade

---
+++ code/m2p_finetune.py
@@ -0,0 +1,319 @@
+import os
+import re
+import math
+import time
+import yaml
+import pickle
+import argparse
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from functools import reduce
+
+from sklearn.metrics import accuracy_score, f1_score
+
+import torch
+import torch.nn as nn
+from torch.nn.utils.rnn import pad_sequence
+
+from transformers import RobertaTokenizer
+from transformers import AdamW
+
+from m2p_mask import process_test_MAM_data
+from m2p_transfer import RobertaM2Downstream
+
+# Here we need to recalculate the metrics for different tasks
+
+def downstream_metrics(pred_label, true_label, task='emotion'):
+    """Calculate the evluation metrics for downstream tasks
+    pred_label: prediction results
+    true_label: human annotations
+    task: the name of the downstream task
+    """
+    assert task in ['emotion','sentiment','verfication']
+    pred_label, true_label = np.array(pred_label), np.array(true_label)
+    
+    if task == 'emotion':
+        wa = np.mean(pred_label.astype(int) == true_label.astype(int))
+        pred_onehot = np.eye(4)[pred_label.astype(int)]
+        true_onehot = np.eye(4)[true_label.astype(int)]
+        ua = np.mean(np.sum((pred_onehot==true_onehot)*true_onehot,axis=0)/np.sum(true_onehot,axis=0))
+        key_metric, report_metric = 0.9*wa+0.1*ua, {'wa':wa,'ua':ua}
+    
+    elif task == 'sentiment':
+        mae = np.mean(np.abs(pred_label - true_label))
+        corr = np.corrcoef(pred_label, true_label)[0][1]
+        nonzero_index = np.where(true_label!=0)
+        pred_2class = (pred_label[nonzero_index] > 0).astype(int)
+        true_2class = (true_label[nonzero_index] > 0).astype(int)
+        acc_2class = accuracy_score(true_2class,pred_2class)
+        f1_2class = f1_score(true_2class, pred_2class, average='weighted')
+        key_metric, report_metric = -1.0 * mae, {'mae':mae,'corr':corr,'acc_2class':acc_2class,'f1_2class':f1_2class}
+
+    else:
+        acc = np.mean(pred_label == true_label)
+        key_metric, report_metric = acc, {'acc':acc}
+
+    return key_metric, report_metric
+
+
+class DownstreamDataset(object):
+    def __init__(self, data_list, tokenizer, audio_length=None):
+        self.data_list = data_list
+        self.audio_length = audio_length
+        self.tokenizer = tokenizer
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, index):
+        audio_path, asr_text, label = self.data_list[index]
+        audio_name = re.sub('\.npy','.wav',os.path.basename(audio_path))
+        audio_input = torch.FloatTensor(np.load(audio_path))
+        if self.audio_length is not None: audio_input=audio_input[:self.audio_length,:]
+        text_words = [x.lower() for x in re.split(' +',re.sub('[\.,\?\!]',' ', asr_text))]
+        text_input = self.tokenizer(' '.join(text_words))
+        return {'audio_input':audio_input,'text_input':text_input,'label':label,'audio_name':audio_name}
+
+
+def collate(sample_list, tokenizer, config):
+    batch_audio = [x['audio_input'] for x in sample_list]
+    pad_batch_audio = pad_sequence(batch_audio, batch_first=True)
+    
+    pad_batch_text = {
+        'input_ids':[x['text_input']['input_ids'] for x in sample_list],
+        'attention_mask':[x['text_input']['attention_mask'] for x in sample_list],
+    }
+    pad_batch_text = tokenizer.pad(pad_batch_text, return_tensors='pt')
+    s_inputs = pad_batch_text['input_ids']
+    s_attention_mask = pad_batch_text['attention_mask']
+    
+    a_attention_mask, a_inputs = process_test_MAM_data((pad_batch_audio,),config)
+    
+    batch_label = torch.tensor([x['label'] for x in sample_list], dtype=torch.long)
+    batch_name = [x['audio_name'] for x in sample_list]
+    return ((a_inputs, a_attention_mask), 
+            (s_inputs, s_attention_mask),
+            batch_label, batch_name)
+
+
+def run(args, config, train_data, valid_data, test_data=None):
+    ############################ PARAMETER SETTING ##########################
+    num_workers = config['dataloader']['n_jobs']
+    batch_size = config['dataloader']['batch_size']
+    # learning_rate = config['optimizer']['learning_rate']
+    # warmup_proportion = config['optimizer']['warmup_proportion']
+    # save_ckpt_dir = os.path.join(args.save_path, 'checkpoints')
+
+    audio_length = 3000
+    epochs = args.epochs
+
+    tokenizer = RobertaTokenizer.from_pretrained(config['upstream']['semantic']['tokenizer_path'])
+    ############################## PREPARE DATASET ##########################
+    train_dataset = DownstreamDataset(train_data, tokenizer, audio_length)
+    train_loader = torch.utils.data.DataLoader(
+        dataset = train_dataset, batch_size = batch_size, 
+        collate_fn=lambda x: collate(x,tokenizer,config['upstream']['acoustic']),
+        shuffle = True, num_workers = num_workers
+    )
+    valid_dataset = DownstreamDataset(valid_data, tokenizer, audio_length)
+    valid_loader = torch.utils.data.DataLoader(
+        dataset = valid_dataset, batch_size = batch_size, 
+        collate_fn=lambda x: collate(x,tokenizer,config['upstream']['acoustic']),
+        shuffle = False, num_workers = num_workers
+    )
+    
+    if test_data is None:
+        test_data = valid_data
+    test_dataset = DownstreamDataset(test_data, tokenizer, audio_length)
+    test_loader = torch.utils.data.DataLoader(
+        dataset = test_dataset, batch_size = batch_size, 
+        collate_fn=lambda x: collate(x,tokenizer,config['upstream']['acoustic']),
+        shuffle = False, num_workers = num_workers
+    )
+    ########################### CREATE MODEL #################################
+    model = RobertaM2Downstream(config['upstream']['ckpt_path'],config['downstream']['label_num'],
+                                freeze=config['upstream']['freeze'],
+                                orthogonal_fusion=config['downstream']['orthogonal_fusion'])
+    model.cuda()
+
+    no_decay = ['bias', 'LayerNorm.weight']
+    optimizer_grouped_parameters = [
+        {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+        {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+    ]
+    optimizer = AdamW(optimizer_grouped_parameters, lr=1e-5)
+
+    ########################### TRAINING #####################################
+    count, best_metric, save_metric, best_epoch = 0, -np.inf, None, 0
+
+    for epoch in range(epochs):
+        epoch_train_loss = []
+        model.train()
+        start_time = time.time()
+
+        progress = tqdm(train_loader, desc='Epoch {:0>3d}'.format(epoch))
+        for acoustic_inputs, semantic_inputs, label_inputs, _ in progress:
+            a_inputs = acoustic_inputs[0].cuda()
+            a_attention_mask = acoustic_inputs[1].cuda()
+            s_inputs = semantic_inputs[0].cuda()
+            s_attention_mask = semantic_inputs[1].cuda()
+            
+            label_inputs = label_inputs.cuda()
+            
+            model.zero_grad()
+            logits, loss = model(
+                s_inputs=s_inputs,
+                s_attention_mask=s_attention_mask,
+                a_inputs=a_inputs,
+                a_attention_mask=a_attention_mask,
+                labels=label_inputs,
+            )
+
+            epoch_train_loss.append(loss)
+
+            loss.backward()
+            optimizer.step()
+
+            count += 1
+            
+            acc_train_loss = torch.mean(torch.tensor(epoch_train_loss)).cpu().detach().numpy()
+            progress.set_description("Epoch {:0>3d} - Loss {:.4f}".format(epoch, acc_train_loss))
+
+        model.eval()
+        pred_y, true_y = [], []
+        for acoustic_inputs, semantic_inputs, label_inputs, _ in valid_loader:
+            a_inputs = acoustic_inputs[0].cuda()
+            a_attention_mask = acoustic_inputs[1].cuda()
+            s_inputs = semantic_inputs[0].cuda()
+            s_attention_mask = semantic_inputs[1].cuda()
+
+            true_y.extend(list(label_inputs.numpy()))
+            
+            logits, _ = model(
+                s_inputs=s_inputs,
+                s_attention_mask=s_attention_mask,
+                a_inputs=a_inputs,
+                a_attention_mask=a_attention_mask,
+                labels=None
+            )
+
+            # prediction = torch.argmax(logits, axis=1)
+            # label_outputs = prediction.cpu().detach().numpy().astype(float)
+
+            if model.label_num == 1:
+                prediction = logits.view(-1)
+                label_outputs = prediction.cpu().detach().numpy().astype(float)
+            else:
+                prediction = torch.argmax(logits, axis=1)
+                label_outputs = prediction.cpu().detach().numpy().astype(int)
+
+            pred_y.extend(list(label_outputs))
+
+        # think about the metric calculation
+        key_metric, report_metric = downstream_metrics(pred_y, true_y, args.task_name)
+        
+        epoch_train_loss = torch.mean(torch.tensor(epoch_train_loss)).cpu().detach().numpy()
+
+        elapsed_time = time.time() - start_time
+        print("The time elapse of epoch {:03d}".format(epoch) + " is: " + 
+                time.strftime("%H: %M: %S", time.gmtime(elapsed_time)))
+        print('Valid Metric: {} - Train Loss: {:.3f}'.format(
+            ' - '.join(['{}: {:.3f}'.format(key, value) for key, value in report_metric.items()]),
+            epoch_train_loss))
+
+        if key_metric > best_metric:
+            best_metric, best_epoch = key_metric, epoch
+            print('Better MAE found on dev, calculate performance on Test')
+            pred_y, true_y = [], []
+            for acoustic_inputs, semantic_inputs, label_inputs, _ in test_loader:
+                a_inputs = acoustic_inputs[0].cuda()
+                a_attention_mask = acoustic_inputs[1].cuda()
+                s_inputs = semantic_inputs[0].cuda()
+                s_attention_mask = semantic_inputs[1].cuda()
+
+                true_y.extend(list(label_inputs.numpy()))
+
+                logits, _, = model(
+                    s_inputs=s_inputs,
+                    s_attention_mask=s_attention_mask,
+                    a_inputs=a_inputs,
+                    a_attention_mask=a_attention_mask,
+                    labels=None
+                )
+                
+                if model.label_num == 1:
+                    prediction = logits.view(-1)
+                    label_outputs = prediction.cpu().detach().numpy().astype(float)
+                else:
+                    prediction = torch.argmax(logits, axis=1)
+                    label_outputs = prediction.cpu().detach().numpy().astype(int)
+                
+                pred_y.extend(list(label_outputs))
+
+            _, save_metric = downstream_metrics(pred_y, true_y, args.task_name)
+            print("Test Metric: {}".format(
+                ' - '.join(['{}: {:.3f}'.format(key, value) for key, value in save_metric.items()])
+            ))
+            
+    print("End. Best epoch {:03d}: {}".format(best_epoch, ' - '.join(['{}: {:.3f}'.format(key, value) for key, value in save_metric.items()])))
+    return save_metric
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--task_name", type=str, default=None, help='downstream task name')
+    parser.add_argument("--config", type=str, default=None, help='configuration file path')
+    parser.add_argument("--epochs", type=int, default=20, help="training epoches")
+    parser.add_argument("--save_path", type=str, default=None, help="report or ckpt save path")
+    parser.add_argument("--freeze", type=bool, default=False, help="freeze the pretrain model")
+
+    args = parser.parse_args()
+
+    config = yaml.load(open(args.config, 'r'), Loader=yaml.FullLoader)
+    
+    report_result = []
+
+    if args.task_name == 'emotion':
+        
+        data_root = '/dataset/iemocap_mel160/V1_0/iemocap_mel160/'
+        data_source = ['/dataset/iemocap_mel160/V1_0/iemocap_mel160/Session1.pkl',
+                       '/dataset/iemocap_mel160/V1_0/iemocap_mel160/Session2.pkl',
+                       '/dataset/iemocap_mel160/V1_0/iemocap_mel160/Session3.pkl',
+                       '/dataset/iemocap_mel160/V1_0/iemocap_mel160/Session4.pkl',
+                       '/dataset/iemocap_mel160/V1_0/iemocap_mel160/Session5.pkl']
+        
+        for i in range(5):
+            valid_name = os.path.basename(data_source[i]).split('.pkl')[0]
+            valid_data = pickle.load(open(data_source[i],'rb'))
+            valid_data = [(os.path.join(data_root,x[0]),x[1],x[2]) for x in valid_data]
+
+            train_data = data_source[:i] + data_source[i+1:]
+            train_data = list(reduce(lambda a,b: a+b, [pickle.load(open(x, 'rb')) for x in train_data]))
+            train_data = [(os.path.join(data_root,x[0]),x[1],x[2]) for x in train_data]
+
+            report_metric = run(args, config, train_data, valid_data, test_data=None)
+            report_result.append(report_metric)
+
+    elif args.task_name == 'sentiment':
+        data_root = '/dataset/mosei_mel160/V1_0/mel160/mel_160/'
+        
+        train_data = pickle.load(open('/dataset/mosei_mel160/V1_0/mel160/train.pkl','rb'))
+        train_data = [(os.path.join(data_root,x[0]),x[1],x[2]) for x in train_data]
+            
+        valid_data = pickle.load(open('/dataset/mosei_mel160/V1_0/mel160/dev.pkl','rb'))
+        valid_data = [(os.path.join(data_root,x[0]),x[1],x[2]) for x in valid_data]
+
+        test_data = pickle.load(open('/dataset/mosei_mel160/V1_0/mel160/test.pkl','rb'))
+        test_data = [(os.path.join(data_root,x[0]),x[1],x[2]) for x in test_data]
+
+        report_metric = run(args, config, train_data, valid_data, test_data)
+        report_result = [report_metric]
+
+    else:
+        # This line need to be finished
+        report_result = []
+
+    # Here we will save the final reports
+    os.makedirs(args.save_path, exist_ok=True)
+    pickle.dump(report_result, open(os.path.join(args.save_path, '{}_report.pkl'.format(args.task_name)),'wb'))
---
+++ code/m2p_transfer.py
@@ -0,0 +1,245 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from transformers import RobertaConfig, RobertaModel
+from m2p_model import AcousticModel
+
+
+class RobertaM2Upstream(nn.Module):
+    def __init__(self, ckpt_path):
+        super().__init__()
+        # First reinitialize the model
+        ckpt_states = torch.load(ckpt_path, map_location='cpu')
+        self.acoustic_config = RobertaConfig(**ckpt_states['Settings']['Config']['acoustic'])
+        self.semantic_config = RobertaConfig(**ckpt_states['Settings']['Config']['semantic'])
+
+        acoustic_model = AcousticModel(self.acoustic_config)
+        semantic_model = RobertaModel(self.semantic_config, add_pooling_layer=False)
+
+        # load the model from pretrained states
+        self.acoustic_model = self.load_model(acoustic_model, ckpt_states['acoustic_model'], 'acoustic.')
+        self.semantic_model = self.load_model(semantic_model, ckpt_states['semantic_model'], 'roberta.')
+
+    def load_model(self, transformer_model, state_dict, prefix_name=''):
+        try:
+            old_keys = []
+            new_keys = []
+            for key in state_dict.keys():
+                new_key = None
+                if 'gamma' in key:
+                    new_key = key.replace('gamma', 'weight')
+                if 'beta' in key:
+                    new_key = key.replace('beta', 'bias')
+                if new_key:
+                    old_keys.append(key)
+                    new_keys.append(new_key)
+            for old_key, new_key in zip(old_keys, new_keys):
+                state_dict[new_key] = state_dict.pop(old_key)
+
+            missing_keys = []
+            unexpected_keys = []
+            error_msgs = []
+            # copy state_dict so _load_from_state_dict can modify it
+            metadata = getattr(state_dict, '_metadata', None)
+            state_dict = state_dict.copy()
+            if metadata is not None:
+                state_dict._metadata = metadata
+
+            def load(module, prefix=''):
+                local_metadata = {} if metadata is None else metadata.get(prefix[:-1], {})
+                module._load_from_state_dict(
+                    state_dict, prefix, local_metadata, True, missing_keys, unexpected_keys, error_msgs)
+                for name, child in module._modules.items():
+                    if child is not None:
+                        load(child, prefix + name + '.')
+
+            load(transformer_model, prefix_name)
+            if len(missing_keys) > 0:
+                print('Weights of {} not initialized from pretrained model: {}'.format(
+                    transformer_model.__class__.__name__, missing_keys))
+            if len(unexpected_keys) > 0:
+                print('Weights from pretrained model not used in {}: {}'.format(
+                    transformer_model.__class__.__name__, unexpected_keys))
+            if len(error_msgs) > 0:
+                raise RuntimeError('Error(s) in loading state_dict for {}:\n\t{}'.format(
+                                    transformer_model.__class__.__name__, '\n\t'.join(error_msgs)))
+            print('[Transformer] - {} Pre-trained weights loaded!'.format(prefix_name))
+            return transformer_model
+
+        except: 
+            raise RuntimeError('[Transformer] - {} Pre-trained weights NOT loaded!'.format(prefix_name))
+
+    def _forward(self,
+                 s_inputs=None,
+                 s_attention_mask=None,
+                 s_token_type_ids=None,
+                 s_position_ids=None,
+                 s_head_mask=None,
+                 a_inputs=None,
+                 a_attention_mask=None,
+                 a_token_type_ids=None,
+                 a_position_ids=None,
+                 a_head_mask=None,
+                 output_attentions=False,
+                 output_hidden_states=False,
+                 return_dict=None):
+        
+        semantic_outputs = self.semantic_model(
+            s_inputs,
+            attention_mask=s_attention_mask,
+            token_type_ids=s_token_type_ids,
+            position_ids=s_position_ids,
+            head_mask=s_head_mask,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        semantic_encode = semantic_outputs[0]
+
+        acoustic_outputs = self.acoustic_model(
+            a_inputs,
+            attention_mask=a_attention_mask,
+            token_type_ids=a_token_type_ids,
+            position_ids=a_position_ids,
+            head_mask=a_head_mask,
+            encoder_hidden_states=semantic_encode,
+            encoder_attention_mask=s_attention_mask,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        acoustic_encode = acoustic_outputs[0]
+
+        return semantic_encode, acoustic_encode
+
+
+class RobertaM2Downstream(RobertaM2Upstream):
+    def __init__(self, ckpt_path, label_num, drop_rate=0.2, freeze=False, orthogonal_fusion=True):
+        super().__init__(ckpt_path)
+        self.freeze = freeze
+        self.label_num = label_num
+        self.orthogonal_fusion = orthogonal_fusion
+        
+        self.acoustic_linear = nn.Linear(self.acoustic_config.hidden_size,self.acoustic_config.hidden_size)
+        self.semantic_linear = nn.Linear(self.semantic_config.hidden_size,self.semantic_config.hidden_size)
+
+        self.acoustic_attention = nn.Linear(self.acoustic_config.hidden_size,1)
+
+        self.fuse_linear = nn.Linear(self.acoustic_config.hidden_size+self.semantic_config.hidden_size, 
+                                     self.acoustic_config.hidden_size+self.semantic_config.hidden_size)
+
+        self.classifier = nn.Linear(self.acoustic_config.hidden_size+self.semantic_config.hidden_size, 
+                                    self.label_num, bias=False)
+
+        self.dropout = nn.Dropout(drop_rate)
+
+        if self.label_num == 1:
+            self.loss_fct = nn.L1Loss()
+        else:
+            self.loss_fct = nn.CrossEntropyLoss()
+
+    def forward(self, 
+                s_inputs=None,
+                s_attention_mask=None,
+                s_token_type_ids=None,
+                s_position_ids=None,
+                s_head_mask=None,
+                a_inputs=None,
+                a_attention_mask=None,
+                a_token_type_ids=None,
+                a_position_ids=None,
+                a_head_mask=None,
+                labels=None,
+                output_attentions=False,
+                output_hidden_states=False,
+                return_dict=None):
+        
+        if self.freeze:
+            with torch.no_grad():
+                semantic_encode, acoustic_encode = self._forward(
+                    s_inputs,
+                    s_attention_mask,
+                    s_token_type_ids,
+                    s_position_ids,
+                    s_head_mask,
+                    a_inputs,
+                    a_attention_mask,
+                    a_token_type_ids,
+                    a_position_ids,
+                    a_head_mask,
+                    output_attentions,
+                    output_hidden_states,
+                    return_dict)
+        else:
+            semantic_encode, acoustic_encode = self._forward(
+                s_inputs,
+                s_attention_mask,
+                s_token_type_ids,
+                s_position_ids,
+                s_head_mask,
+                a_inputs,
+                a_attention_mask,
+                a_token_type_ids,
+                a_position_ids,
+                a_head_mask,
+                output_attentions,
+                output_hidden_states,
+                return_dict)
+
+        semantic_encode = self.semantic_linear(semantic_encode)
+        semantic_encode = torch.tanh(semantic_encode)
+
+        semantic_pool = semantic_encode[:,0,:]
+
+        semantic_encode = semantic_encode * s_attention_mask[:,:,None] - 10000.0 * (1.0 - s_attention_mask[:,:,None])
+        semantic_max = torch.max(semantic_encode[:,1:,:],dim=1)[0]
+
+        acoustic_encode = self.acoustic_linear(acoustic_encode)
+        acoustic_encode = torch.tanh(acoustic_encode)
+
+        acoustic_pool = self.acoustic_attention(acoustic_encode)
+        acoustic_pool = acoustic_pool * a_attention_mask[:,:,None] - 10000.0 * (1.0 - a_attention_mask[:,:,None])
+        acoustic_pool = F.softmax(acoustic_pool, dim=1)
+        acoustic_pool = torch.matmul(acoustic_pool.permute(0,2,1), acoustic_encode).squeeze(1)
+
+        acoustic_encode = acoustic_encode * a_attention_mask[:,:,None] - 10000.0 * (1.0 - a_attention_mask[:,:,None])
+        acoustic_max = torch.max(acoustic_encode, dim=1)[0]
+
+        if self.orthogonal_fusion:
+            att_feats = semantic_pool + acoustic_pool
+            max_feats = semantic_max + acoustic_max
+            fuse_encode = torch.cat([att_feats, max_feats], dim=-1)
+        else:
+            fuse_encode = torch.cat([semantic_pool, acoustic_pool],dim=-1)
+
+        fuse_encode = self.fuse_linear(fuse_encode)
+        fuse_encode_act = torch.tanh(fuse_encode)
+
+        logits = self.classifier(fuse_encode_act)
+        
+        if labels is not None:
+            if self.label_num == 1:
+                loss = self.loss_fct(logits.view(-1), labels.view(-1))
+            else:
+                loss = self.loss_fct(logits, labels)
+
+            if self.orthogonal_fusion:
+                # This line could be different with the paper?
+                acoustic_state = acoustic_pool + acoustic_max
+                semantic_state = semantic_pool + semantic_max
+                
+                acoustic_norm = torch.norm(acoustic_state,p=2,dim=1)
+                semantic_norm = torch.norm(acoustic_state,p=2,dim=1)
+
+                orth_loss = torch.diag(torch.matmul(acoustic_state, semantic_state.permute(1,0)))
+                orth_loss = torch.mean(torch.abs(orth_loss / (acoustic_norm * semantic_norm)))
+
+                loss += orth_loss
+
+        else:
+            loss = None
+
+        return logits, loss
